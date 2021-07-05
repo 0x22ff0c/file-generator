e49c913b7911e35a5f23dbc1c5cd09993f5d213d
0x22ff0c <yiibui.96@gmail.com>
2021-07-06 04:11:10 +0800

    Add validations class

---
+++ src/com/devicelist/Validations.java
@@ -0,0 +1,89 @@
+package com.devicelist;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Validations{
+	
+	public String numberInput = ""; 
+	private boolean isDigit = false;
+	private boolean isInRange = false;
+	
+	public void checkIfInputIsDigit(String testData){
+	
+		numberInput = testData;
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		if(!testData.matches("\\d+")){
+	
+			while(isDigit == false){
+
+				System.out.println("Error: Input is not a digit.");
+				
+				System.out.print("Enter a number: ");
+				
+				try {
+					numberInput = reader.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				if(numberInput.matches("\\d+")){
+					isDigit = true;
+				}
+			}
+			
+		}
+	}
+	
+	public void checkIfInputIsInRange(){
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			
+			int rangeNumber = Integer.parseInt(numberInput);
+			
+			if(rangeNumber == 0 || rangeNumber > 10){
+				
+				while(isInRange == false){
+					System.out.println("Error: Number should be between 1 and 9.");
+				
+					System.out.print("Enter a number between 1 and 9: ");
+					
+					numberInput = reader.readLine();
+					
+					rangeNumber = Integer.parseInt(numberInput);
+					
+					if(rangeNumber != 0 && rangeNumber < 10){
+						isInRange = true;
+					}
+				}
+			}
+		
+		} catch (NumberFormatException numberFormatException){
+			checkIfInputIsDigit(numberInput);
+			isDigit = false;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void checkIfInputIsDigitAndInRange(String testData){
+		
+		numberInput = testData;
+		
+		while (isDigit == false && isInRange == false){
+			
+			if(isDigit == false){
+				checkIfInputIsDigit(numberInput);
+			}
+			
+			if(isInRange == false){
+				checkIfInputIsInRange();
+			}
+		}	
+	}
+}
