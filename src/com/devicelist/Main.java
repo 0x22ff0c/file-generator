c44d61b36894984358af92b8d85161015e6ad3b9
0x22ff0c <yiibui.96@gmail.com>
2021-07-08 06:46:18 +0800

    Removed unnecessary method cals

--- src/com/devicelist/Main.java
+++ src/com/devicelist/Main.java
@@ -8,7 +8,8 @@ public class Main {
 		
 		fileManager.generateFile();
 		
-		fileManager.addHeader("Sample text header");
+		fileManager.addHeader("Grocery list items:");
 
+		fileManager.promptToInputAValue();
 	}
 }
